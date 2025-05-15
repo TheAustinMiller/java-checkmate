a5621e6a0262075d76267b1b330e5f12a1dd22cf
Austin Miller <113201387+TheAustinMiller@users.noreply.github.com>
2025-05-15 12:20:45 -0500

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,18 @@
+public class Card {
+    private int val;
+
+    public Card(int val) {
+        this.val = val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public int getVal() { return this.val; }
+
+    @Override
+    public String toString() {
+        return val + "";
+    }
+}
